63b67dfc5a17bd2da546f62a314a0f05c187f6ad
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2024-10-06 21:53:37 +0600

    added project

--- fibbonacci.java
+++ fibbonacci.java
@@ -2,14 +2,13 @@ public class fibbonacci {
     public static void main(String[] args){
         int pre1=0;
         int pre2=1;
-        int iter=100;
+        int iter=10;
         int current=pre2;
         for (int i=0;i<=iter;i++){
             System.out.println(current+", ");
             current=pre1+pre2;
             pre1=pre2;
             pre2=current;
-
         }
     }
 }
