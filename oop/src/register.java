3b962d7e75cbefa86c27a92679523a9f03bb8fdf
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2024-10-18 23:44:44 +0600

    new work on oop and rotator

---
+++ oop/src/register.java
@@ -0,0 +1,38 @@
+class student_register{
+    String name;
+    int id;
+    float stipend;
+    student_register(){
+        System.out.println("you did  nothing");
+    }
+    student_register(String name,int id){
+        this.id=id;
+        this.name=name;
+    }
+    student_register(String name,int id,float stipend){
+        this.id=id;
+        this.name=name;
+        this.stipend=stipend;
+    }
+
+    public void display(){
+        System.out.print("Name : "+this.name+"\n"+"Id : "+this.id+"\n"+"Stipend : "+this.stipend+"\n\n");
+    }
+}
+
+
+
+public class register   {
+    public static void main(String[] args) {
+        student student1= new student();
+        student student2=new student("Nishan",1);
+        student student3=new student("Ishan",2, 0.2F);
+
+        //print the details of the method overloading
+
+        student1.display();
+        student2.display();
+        student3.display();
+    }
+}
+
