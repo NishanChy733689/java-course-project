f90e13993b1f685a2f4368cb989a4681ba3ae113
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2025-02-07 20:16:56 +0600

    The Selection sorting algorithm
    Signed-off-by: NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>

--- Stings/src/stringToArr.java
+++ Stings/src/stringToArr.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class stringToArr {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String word = scn.next();
+        StringBuilder builder=new StringBuilder(word);
+        builder.reverse();
+        word=builder.toString();
+        char[] arr=word.toCharArray();
+        System.out.println(arr);
+
+    }
 }
