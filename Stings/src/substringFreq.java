141937e5f6d9a367d913ec0642c46e5d527720f0
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2025-02-08 21:08:50 +0600

    The Selection sorting algorithm
    Signed-off-by: NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>

--- Stings/src/substringFreq.java
+++ Stings/src/substringFreq.java
@@ -18,7 +18,26 @@ public class substringFreq{
         return counter;
     }
 
+    public static String replaceSubstring(String a,String b,String replace){
+        if (b.length()>a.length()){
+            return "error";
+        }
+        String res="";
+        for (int i=0;i<a.length();i++){
+            int start=i;
+            for(int j=i+1;j<a.length();j++){
+                String slice=a.substring(start,j);
+                if (slice.equalsIgnoreCase(b)){
+                    res=a.replace(slice,replace);
+                    System.out.println(res);
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println(countSubFreq("Supercalifragilisticexpialidocious","li"));
+        System.out.println(replaceSubstring("Supercalifragilisticexpialidocious","Super","superman"));
     }
 }
