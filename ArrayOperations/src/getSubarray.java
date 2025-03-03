7a7fc2027774b0956c1d86403688a5991cd47f93
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2025-03-03 19:27:53 +0600

    works
    Signed-off-by: NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>

---
+++ ArrayOperations/src/getSubarray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class getSubarray {
+    public static int[] getSub(int[] arr,int start,int end){
+        int[] sub=new int[end-start+1];
+        int j=0;
+        for(int i=start;i<=end;i++){
+            sub[j]=arr[i];
+            j++;
+        }
+        return sub;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9,10,11};
+        System.out.println(Arrays.toString(getSub(arr, 0, 9)));
+    }
+}
