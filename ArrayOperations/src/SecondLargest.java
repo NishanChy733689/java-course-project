9c5e510fb3a72420fdc38d8ee30760622ce2a787
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2025-03-11 09:48:50 +0600

    Add findFactors and Comment_formatter classes to ArrayOperations
    - Implement findFactors class to calculate and return factors of a given number.
    - Create Comment_formatter class to format comments with numbering and punctuation.
    - Include main methods in both classes for testing functionality.
    
    Signed-off-by: NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>

--- ArrayOperations/src/SecondLargest.java
+++ ArrayOperations/src/SecondLargest.java
@@ -1,11 +1,43 @@
-public class SecondLargest {
+import java.util.Arrays;
 
+public class SecondLargest {
+    public static void main(String[] args) {
+        //Define the array
+        int[] array={1,3,6,8,4,5,6};
+        // Sort the array so the second large number is the second last element
+        Arrays.sort(array);
+
+        //Create the tracker object
+        numTracker tracker=new numTracker();
+
+        //Iterate through the array and keep updating the tracker
+        for(int i=0;i<array.length;i++){
+            int curr=array[i];
+            tracker.update(curr);
+        }
+
+        //print the results
+        System.out.println(tracker.getLastHigh());
+    }
 }
 
 class numTracker{
-    private int current;
-    private int lastHigh;
-    public static void update(int newInt){
+    // Make two pointers
+    private static int current;
+    private static int lastHigh;
 
+    public static void update(int newInt){
+        // store the previous pinter value
+        int temp=current;
+        // update the pointer current
+        current=newInt;
+        //update the pointer lastHigh
+        if (current>temp){
+            lastHigh=temp;
+        }
+    }
+    //the getter method for lastHigh
+    public static int getLastHigh(){
+        return lastHigh;
     }
 }
--- ArrayOperations/src/SubsetFInder.java
+++ ArrayOperations/src/SubsetFInder.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import java.util.List;
+
 public class SubsetFInder {
 
-    static Set<List<Integer>> output=new HashSet<>();
+    Set<List<Integer>> output=new HashSet<>();
 
-    public static List<List<Integer>> subsets(int[] arr){
+    public  List<List<Integer>> subsets(int[] arr){
         int n=arr.length;
 
         Arrays.sort(arr);
@@ -15,7 +16,7 @@ public class SubsetFInder {
 
         return answer;
     }
-    public static void help(int first, ArrayList<Integer> curr, int[] array){
+    public void help(int first, ArrayList<Integer> curr, int[] array){
         if (first==array.length){
 
                 output.add(new ArrayList<>(curr));
@@ -29,9 +30,9 @@ public class SubsetFInder {
 
     }
 
-    public static void main(String[] args) {
-        int[] nums={1,2,3,2,3,2};
-        List<List<Integer>> result= subsets(nums);
-        System.out.println(result);
-    }
+//    public static void main(String[] args) {
+//        int[] nums={1,2,3,2,3,2};
+//        List<List<Integer>> result= subsets(nums);
+//        System.out.println(result);
+//    }
 }
